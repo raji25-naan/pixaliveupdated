61ae1c373a4d5a878dd5a04a52137ddf8fe64db0
pixmash <awspixmash@pixalive.me>
2021-09-20 19:26:19 +0530

    1000 again ch to live

--- api/controllers/User/points.js
+++ api/controllers/User/points.js
@@ -87,7 +87,7 @@ module.exports.trendingPeople = async (req,res,next)=>{
             var friendAndRequest = following_data.concat(request_data,inactiveUsers);
             
             //getTrendingPeople
-            const getTrendingPeople = await pointSchema.find({_id:{$nin: friendAndRequest}}).populate("_id","name username avatar private followersCount follow").sort({total_Points: -1}).limit(500).exec();
+            const getTrendingPeople = await pointSchema.find({_id:{$nin: friendAndRequest}}).populate("_id","name username avatar private followersCount follow").sort({total_Points: -1}).limit(1000).exec();
             if(getTrendingPeople)
             {
                 return res.json({
@@ -108,7 +108,7 @@ module.exports.trendingPeople = async (req,res,next)=>{
         }
         else
         {
-            const trendingPeopleList = await pointSchema.find({_id:{$nin: inactiveUsers}}).populate("_id","name username avatar private followersCount follow").sort({total_Points: -1}).limit(500).exec();
+            const trendingPeopleList = await pointSchema.find({_id:{$nin: inactiveUsers}}).populate("_id","name username avatar private followersCount follow").sort({total_Points: -1}).limit(1000).exec();
             let getTrendingPeople;
             if(req.query.search)
             {
